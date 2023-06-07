ae9c663ab612210b23a497c448bf378c2f8eba5a
antoninodangelo11 <reapercsp@gmail.com>
2023-06-07 17:54:47 +0200

    add select

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<!-- ARRAY PHP -->
+
 <?php
+
+    function strbool($hotel_i) {
+        if (is_bool($hotel_i)) {
+            return $hotel_i ? 'Si' : 'No';
+        }
+        return $hotel_i;
+    }
+
+    echo strbool(false);
+    echo strbool(true);
 
     $hotels = [
 
@@ -42,7 +54,6 @@
 
 ?>
 
-
 <!DOCTYPE html>
 <html lang="it">
 <head>
@@ -50,14 +61,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    
+    <!-- BOOSTRAP LINK/SCRIPT -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
-<body>
-    <div class="container">
+<body class="bg-dark">
+    <div class="container bg-body-tertiary py-5">
+        
+        <h1 class="fw-bold">LISTA HOTELBOOL</h1>
+        
+        <!-- FILTER HOTELS -->
+        <form class="d-flex gap-2" method="GET">
+            <select class="form-select w-25 mb-3" aria-label="Default select example">
+                <option selected>Tutti</option>
+                <option value="1">Con Parcheggio</option>
+                <option value="2">Senza Parcheggio</option>
+            </select>
+
+            <select class="form-select w-25 mb-3" aria-label="Default select example">
+                <option selected>Tutti</option>
+                <option value="1">Voto 1</option>
+                <option value="2">Voto 2</option>
+                <option value="3">Voto 3</option>
+                <option value="4">Voto 4</option>
+                <option value="5">Voto 5</option>
+            </select>
+
+            <div class="d-grid gap-2 d-md-block">
+                <button class="btn btn-primary" type="submit">Cerca</button>
+                <button class="btn btn-danger" type="reset">Cancella</button>
+            </div>  
+        </form>
+        
+        <!-- HOTELS TABLE -->
         <table class="table">
             <thead>
-                <tr>
+                <tr class="table-dark">
                     <th scope="col">Nome</th>
                     <th scope="col">Descrizione</th>
                     <th scope="col">Parcheggio</th>
@@ -65,14 +106,13 @@
                     <th scope="col">Distanza dal centro</th>
                 </tr>
             </thead>
+            
             <tbody>
                 <?php foreach ($hotels as $hotel) { ?>
                 <tr>
-                    <td><?= $hotel['name'] ?></td>
-                    <td><?= $hotel['description'] ?></td>
-                    <td><?= $hotel['parking'] ?></td>
-                    <td><?= $hotel['vote'] ?></td>
-                    <td><?= $hotel['distance_to_center'] ?></td>
+                <?php foreach ($hotel as $hotel_i) { ?>
+                    <td class="table-success"><?= strbool($hotel_i) ?></td>
+                <?php } ?>
                 </tr>
                 <?php } ?>
             </tbody>
